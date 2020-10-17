103dbdd0feca8cdfe0f07d5e17caf21186b1034c
Jelly-code <santamaria.javiera@gmail.com>
2020-10-17 14:46:46 -0300

    filtro por categoria funcionando

--- main.js
+++ main.js
@@ -16,3 +16,44 @@ busquedaProducto.oninput = () => {
         }
     }
 }
+
+// ** FILTRO POR CATEGORIA **
+
+const filtroCategoria = document.querySelectorAll(".filtro-categoria")
+console.log(filtroCategoria)
+for (let checkbox of filtroCategoria) {
+    checkbox.oninput = () => {
+        filtrarProductos();
+    }
+}
+
+const hayCheckboxSeleccionado = () => {
+    for (let checkbox of filtroCategoria) {
+        if (checkbox.checked) {
+            return true
+        }
+    }
+}
+
+const coincideCheckboxYProducto = producto => {
+    const category = producto.dataset.category;
+    for (let checkbox of filtroCategoria) {
+        if (checkbox.value === category && checkbox.checked) {
+            return true
+        }
+    }
+}
+
+const filtrarProductos = () => {
+    for (let producto of productos) {
+        producto.classList.add('hidden')
+        if (hayCheckboxSeleccionado()) {
+            if (coincideCheckboxYProducto(producto)) {
+                producto.classList.remove('hidden')
+            }
+        }
+        else {
+            producto.classList.remove('hidden')
+        }
+    }
+}
